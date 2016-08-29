f642da3582fb855b2bc341237ea9473b66f9fc86
Drona Baral <Drona Baral>
2016-08-29 12:38:55 -0500

    project 7

---
+++ project7/src/js/arrayFunction.js
@@ -0,0 +1,7 @@
+function findLength (sample){
+    return sample.length;
+}
+function traverseArray (sample){
+   for(var i=0;i<sample.length;i++){
+    document.write(i+" ) "+sample[i]+ "<br>");
+}}
---
+++ project7/src/js/first.js
@@ -0,0 +1,4 @@
+var x=12;
+var y=2;
+console.log(x*y);
+document.write('What the duce');
